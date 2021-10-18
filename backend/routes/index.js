ce3a8871bdaf788e813c25cfa0fbe69de03283bc
Jarum Carr <jarumlcarr@gmail.com>
2021-10-18 00:52:26 -0700

    deploying to heroku

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,18 +2,13 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
-router.get('/', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
-
 router.use('/api', apiRouter);
 
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -22,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
 
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -31,7 +26,7 @@ if (process.env.NODE_ENV === 'production') {
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+    res.status(201).json({});
   });
 }
 
